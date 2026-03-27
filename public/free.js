6f6cc63ee7239c4dd65043ad36560ccc68e6df01
jmlsd75-lang <jmlsd75@gmail.com>
2026-03-27 23:00:22 +0300

    Update free.js

--- public/free.js
+++ public/free.js
@@ -1,16 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Multisystem Management</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
 
-    <div class="container">
-        <h1 id="title"></h1>
-    </div>
-
-    <script src="script.js"></script>
-</body>
-</html>
