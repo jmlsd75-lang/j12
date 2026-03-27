493be8139f4bad6204e337f0df0ba90a91d9b179
jmlsd75-lang <jmlsd75@gmail.com>
2026-03-28 01:08:13 +0300

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,20 +1,20 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Your Firebase project config
+// 🔥 Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyDpNJIZoLeZUhIoTepbLb_3rRLpseu9Zdo",
   authDomain: "my-project-66803-95cb3.firebaseapp.com",
   projectId: "my-project-66803-95cb3",
-  storageBucket: "my-project-66803-95cb3.appspot.com",
+  storageBucket: "my-project-66803-95cb3.firebasestorage.app",
   messagingSenderId: "167159607898",
   appId: "1:167159607898:web:23ca11366b88868b085e63"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
 
-export { auth, provider };
+// Export Firestore instance for use in other files
+export { db };
