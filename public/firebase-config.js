96c60c123c084269a362a60c95c6702b970877db
jmlsd75-lang <jmlsd75@gmail.com>
2026-03-27 22:50:18 +0300

    Create firebase-config.js

---
+++ public/firebase-config.js
@@ -0,0 +1,7 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+export const app = initializeApp({
+  apiKey: "AIzaSyDpNJIZoLeZUhIoTepbLb_3rRLpseu9Zdo",
+  authDomain: "my-project-66803-95cb3.firebaseapp.com",
+  projectId: "my-project-66803-95cb3"
+});
