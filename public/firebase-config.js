afcb590c5018624670d3eb1d72b2f3da08832966
jmlsd75-lang <jmlsd75@gmail.com>
2026-03-27 23:48:24 +0300

    Delete public/firebase-config.js

--- public/firebase-config.js
+++
@@ -1,7 +0,0 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-export const app = initializeApp({
-  apiKey: "AIzaSyDpNJIZoLeZUhIoTepbLb_3rRLpseu9Zdo",
-  authDomain: "my-project-66803-95cb3.firebaseapp.com",
-  projectId: "my-project-66803-95cb3"
-});
