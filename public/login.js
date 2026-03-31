286779c1476155650caa9eea0732e993d39cc994
jmlsd75-lang <jmlsd75@gmail.com>
2026-04-01 00:11:54 +0300

    Update login.js

--- public/login.js
+++ public/login.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
 };
 
 // Put your exact admin email here
-const ADMIN_EMAIL = "jmlsd@gmail.com";
+const ADMIN_EMAIL = "jmlsd75@gmail.com";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
